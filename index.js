4e91b3a60d5726c8667a9a298b011dd423112be0
Thalindriel <dimasikataz@gmail.com>
2025-02-10 12:40:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -131,20 +131,4 @@ document.addEventListener("DOMContentLoaded", function () {
           document.getElementById('registration-page').classList.add('hidden');
         }
       });
-
- const emailInput = document.querySelector('.reg-rectangle input[placeholder="Email"]');
-    const passwordInput = document.querySelector('.reg-rectangle input[placeholder="Пароль"]');
-    const createAccountButton = document.querySelector('.reg-submit');
-
-    if (emailInput && passwordInput && createAccountButton) {
-        function updateButtonState() {
-            if (emailInput.value.trim() !== '' && passwordInput.value.trim() !== '') {
-                createAccountButton.classList.add('active');
-            } else {
-                createAccountButton.classList.remove('active');
-            }
-        }
-        emailInput.addEventListener('input', updateButtonState);
-        passwordInput.addEventListener('input', updateButtonState);
-    }
 });
